d969587b20b530b79abead6f67646a0b8f305234
komorin0521 <haikaradaisuki@gmail.com>
2015-06-04 23:52:13 +0900

    change correctly shell script of registration for github)

--- 20150604_github_regist.sh
+++ 20150604_github_regist.sh
@@ -1,4 +1,6 @@
 cd /home/omori/.ssh
 ssh-keygen -C "haikaradaisuki@gmail.com"
-echo -e "-----\nHost github\n Hostname github.com\n Port 22\n User git \nIdentityFile /home/omori/.ssh/id_rsa\n-----" >> /home/omori/.ssh/config
-chmod 0600 /home/omori/.ssh/github_rsa
+echo -e "Host github\n Hostname github.com\n Port 22\n User komorin0521\n IdentityFile /home/omori/.ssh/github_rsa\n" >> /home/omori/.ssh/config
+chmod 600 /home/omori/.ssh/github_rsa
+chmod 600 /home/omori/.ssh/config
+chmod 700 /home/omori/.ssh
