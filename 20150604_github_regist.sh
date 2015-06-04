42a1b24c5e5d628167bbea7784c0e1464979044f
komorin0521 <haikaradaisuki@gmail.com>
2015-06-04 23:56:13 +0900

    add shell script of registration for github(add ssh -T git@github.com)

--- 20150604_github_regist.sh
+++ 20150604_github_regist.sh
@@ -4,3 +4,4 @@ echo -e "Host github\n Hostname github.com\n Port 22\n User komorin0521\n Identi
 chmod 600 /home/omori/.ssh/github_rsa
 chmod 600 /home/omori/.ssh/config
 chmod 700 /home/omori/.ssh
+ssh -T git@github.com
