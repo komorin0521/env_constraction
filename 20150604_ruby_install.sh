c0ff8aa8215772ac67b677fed583ef1c92c0b383
komorin0521 <haikaradaisuki@gmail.com>
2015-06-05 00:00:12 +0900

    add shell script of installing ruby

---
+++ 20150604_ruby_install.sh
@@ -0,0 +1,6 @@
+#git clone https://github.com/sstephenson/ruby-build.git /home/omori/.rbenv/plugins/ruby-build
+#sudo yum -y install gcc openssl-devel
+rbenv install -v 2.2.2
+rbenv rehash
+rbenv global 2.2.2
+
