5c82f334123b29710aba80e62fa0005c62c82cc1
komorin0521 <haikaradaisuki@gmail.com>
2015-06-06 20:08:51 +0900

    shell script of installing pyenv

---
+++ 20150606_pyenv_install.sh
@@ -0,0 +1,7 @@
+sudo yum -y install gcc bzip2 bzip2-devel openssl openssl-devel readline readline-devel
+git clone git://github.com/yyuu/pyenv.git /home/omori/.pyenv
+echo -e "\n\n#rewrite in 6,June,2015" >> /home/omori/.bash_profile
+echo "export PATH=$HOME/.pyenv/bin:$PATH" >> /home/omori/.bash_profile
+echo -e '\neval "$(pyenv init --no-rehash -)"' >> /home/omori/.bash_profile
+source /home/omori/.bash_profile
+exec $SHELL
