8e479158d85f282f6c47ebedf8058eadf17a9588
komorin0521 <haikaradaisuki@gmail.com>
2015-06-06 20:27:10 +0900

    add installing package sqlite and sqlite-devel

--- 20150606_pyenv_install.sh
+++ 20150606_pyenv_install.sh
@@ -1,4 +1,4 @@
-sudo yum -y install gcc bzip2 bzip2-devel openssl openssl-devel readline readline-devel
+sudo yum -y install gcc bzip2 bzip2-devel openssl openssl-devel readline readline-devel sqlite sqlite-devel
 git clone git://github.com/yyuu/pyenv.git /home/omori/.pyenv
 echo -e "\n\n#rewrite in 6,June,2015" >> /home/omori/.bash_profile
 echo "export PATH=$HOME/.pyenv/bin:$PATH" >> /home/omori/.bash_profile
