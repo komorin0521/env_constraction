e77dc69f02684a969f380248ef79623cff3a00bb
komorin0521 <haikaradaisuki@gmail.com>
2015-08-23 10:20:20 +0900

    modify

--- 20150604_rbenv_install.sh
+++ 20150604_rbenv_install.sh
@@ -1,7 +1,6 @@
-git clone https://github.com/sstephenson/rbenv.git /home/omori/.rbenv
-cd /home/omori/
-echo -e "\n#rewrite in 4,June,2015"
-echo "export PATH=$HOME/.rbenv/bin:$PATH" >> .bash_profile
-echo -e '\neval "$(rbenv init --no-rehash -)"' >> .bash_profile
-source .bash_profile
+git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+echo -e "\n#rewrite in 4,June,2015" >> ~/.bash_profile
+echo "export PATH=$HOME/.rbenv/bin:$PATH" >> ~/.bash_profile
+echo -e '\neval "$(rbenv init --no-rehash -)"' >> ~/.bash_profile
+source ~/.bash_profile
 exec $SHELL
