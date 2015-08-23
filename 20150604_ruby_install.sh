755ae18a9fdab8dfbe5bf3c85110012e137c1f8b
komorin0521 <haikaradaisuki@gmail.com>
2015-08-23 10:24:13 +0900

    modify

--- 20150604_ruby_install.sh
+++ 20150604_ruby_install.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/sstephenson/ruby-build.git /home/omori/.rbenv/plugins/ruby-build
+git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
 sudo yum -y install gcc openssl-devel
 rbenv install -v 2.2.2
 rbenv rehash
