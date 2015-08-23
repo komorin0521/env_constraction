3ba52b8901c5b6066b6e9f1035ba86751c5ea326
komorin0521 <haikaradaisuki@gmail.com>
2015-08-23 09:54:42 +0900

    remove github_regist

--- 20150604_github_regist.sh
+++
@@ -1,6 +0,0 @@
-cd /home/omori/.ssh
-ssh-keygen -C "haikaradaisuki@gmail.com"
-echo -e "Host github\n Hostname github.com\n Port 22\n User komorin0521\n IdentityFile /home/omori/.ssh/github_rsa\n" >> /home/omori/.ssh/config
-chmod 600 /home/omori/.ssh/github_rsa
-chmod 600 /home/omori/.ssh/config
-chmod 700 /home/omori/.ssh
