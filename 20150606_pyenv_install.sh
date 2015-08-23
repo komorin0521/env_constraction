20c62fe8b79321cc0655121fb58f62a896435a46
komorin0521 <haikaradaisuki@gmail.com>
2015-08-23 10:24:57 +0900

    modify

--- 20150606_pyenv_install.sh
+++ 20150606_pyenv_install.sh
@@ -1,7 +1,7 @@
 sudo yum -y install gcc bzip2 bzip2-devel openssl openssl-devel readline readline-devel sqlite sqlite-devel
-git clone git://github.com/yyuu/pyenv.git /home/omori/.pyenv
-echo -e "\n\n#rewrite in 6,June,2015" >> /home/omori/.bash_profile
-echo "export PATH=$HOME/.pyenv/bin:$PATH" >> /home/omori/.bash_profile
-echo -e '\neval "$(pyenv init --no-rehash -)"' >> /home/omori/.bash_profile
-source /home/omori/.bash_profile
+git clone git://github.com/yyuu/pyenv.git ~/.pyenv
+echo -e "\n\n#rewrite in 6,June,2015" >> ~/.bash_profile
+echo "export PATH=$HOME/.pyenv/bin:$PATH" >> ~/.bash_profile
+echo -e '\neval "$(pyenv init --no-rehash -)"' >> ~/.bash_profile
+source ~/.bash_profile
 exec $SHELL
